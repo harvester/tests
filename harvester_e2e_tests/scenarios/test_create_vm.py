b558f611681e70fa229078bc356be4ed6f6d27e6
Charu Jain <cjain@suse.com>
2021-06-25 19:10:10 +0000

    Adding the tests for actions on Created VM

--- harvester_e2e_tests/scenarios/test_create_vm.py
+++ harvester_e2e_tests/scenarios/test_create_vm.py
@@ -18,16 +18,16 @@
 from harvester_e2e_tests import utils
 import pytest
 import time
-
+import json
 
 pytest_plugins = [
-   'harvester_e2e_tests.fixtures.image',
-   'harvester_e2e_tests.fixtures.keypair',
-   'harvester_e2e_tests.fixtures.volume',
-  ]
+    'harvester_e2e_tests.fixtures.image',
+    'harvester_e2e_tests.fixtures.keypair',
+    'harvester_e2e_tests.fixtures.volume',
+]
 
 
-@pytest.fixture()
+@pytest.fixture(scope='class')
 def basic_vm(request, admin_session, image, keypair, volume,
              harvester_api_endpoints):
     request_json = utils.get_json_object_from_template(
@@ -75,5 +75,162 @@ def test_create_vm(admin_session, basic_vm, keypair, harvester_api_endpoints):
         basic_vm['metadata']['name']))
     assert resp.status_code == 200, 'Failed to lookup VM instance %s' % (
         basic_vm['metadata']['name'])
+    vmi_resp_json = resp.json()
+    vmiUID = vmi_resp_json['metadata']['uid']
+
     # TODO(gyee): need to make sure we can SSH into the VM, but only
     # if the VM has a public IP.
+
+    # Test Various actions on created VM
+#    _update_vm(admin_session, harvester_api_endpoints, basic_vm)
+    _restart_vm(admin_session, harvester_api_endpoints,
+                basic_vm, vmiUID)
+    _stop_vm(admin_session, harvester_api_endpoints, basic_vm)
+    _start_vm(admin_session, harvester_api_endpoints, basic_vm)
+    _pause_vm(admin_session, harvester_api_endpoints, basic_vm)
+    _unpause_vm(admin_session, harvester_api_endpoints, basic_vm)
+
+
+def _restart_vm(admin_session, harvester_api_endpoints, basic_vm, vmiid):
+    resp = admin_session.post(harvester_api_endpoints.restart_vm % (
+        basic_vm['metadata']['name']))
+    assert resp.status_code == 204, 'Failed to restart VM instance %s' % (
+        basic_vm['metadata']['name'])
+
+    retries = 20
+    while retries:
+        resp = admin_session.get(harvester_api_endpoints.get_vm_instance % (
+            basic_vm['metadata']['name']))
+        if resp.status_code == 200:
+            resp_json = resp.json()
+            if ('status' in resp_json and
+                    resp_json['status']['phase'] == 'Running' and
+                    resp_json['metadata']['uid'] != vmiid):
+                vmiUID = resp_json['metadata']['uid']
+                break
+
+        time.sleep(30)
+        retries -= 1
+
+    assert resp.status_code == 200, 'Failed to get VM instance %s' % (
+        basic_vm['metadata']['name'])
+    assert vmiid != vmiUID, 'VM instance not changed for %s' % (
+        basic_vm['metadata']['name'])
+
+
+def _stop_vm(admin_session, harvester_api_endpoints, basic_vm):
+    resp = admin_session.post(harvester_api_endpoints.stop_vm % (
+        basic_vm['metadata']['name']))
+    assert resp.status_code == 204, 'Failed to stop VM instance %s' % (
+        basic_vm['metadata']['name'])
+
+    retries = 10
+    while retries:
+        resp = admin_session.get(harvester_api_endpoints.get_vm_instance % (
+            basic_vm['metadata']['name']))
+        if resp.status_code == 200:
+            time.sleep(5)
+            retries -= 1
+        else:
+            break
+
+    assert resp.status_code == 404, 'Found VM instance for: %s' % (
+        basic_vm['metadata']['name'])
+
+
+def _start_vm(admin_session, harvester_api_endpoints, basic_vm):
+    resp = admin_session.post(harvester_api_endpoints.start_vm % (
+        basic_vm['metadata']['name']))
+    assert resp.status_code == 204, 'Failed to start VM instance %s' % (
+        basic_vm['metadata']['name'])
+
+    retries = 10
+    while retries:
+        resp = admin_session.get(harvester_api_endpoints.get_vm_instance % (
+            basic_vm['metadata']['name']))
+        if resp.status_code == 200:
+            resp_json = resp.json()
+            if ('status' in resp_json and
+                    resp_json['status']['phase'] == 'Running'):
+                break
+
+        time.sleep(5)
+        retries -= 1
+
+    assert resp.status_code == 200, 'Failed to get VM instance for: %s' % (
+        basic_vm['metadata']['name'])
+
+
+def _pause_vm(admin_session, harvester_api_endpoints, basic_vm):
+    paused = False
+    resp = admin_session.post(harvester_api_endpoints.pause_vm % (
+        basic_vm['metadata']['name']))
+    assert resp.status_code == 204, 'Failed to pause VM instance %s' % (
+        basic_vm['metadata']['name'])
+
+    retries = 10
+    while retries:
+        resp = admin_session.get(harvester_api_endpoints.get_vm % (
+            basic_vm['metadata']['name']))
+        if resp.status_code == 200:
+            resp_json = resp.json()
+            if ('status' in resp_json and
+                    'Paused' in [i['type'] for i in
+                                 resp_json['status']['conditions']] and
+                    "True" in [i['status'] for i in
+                               resp_json['status']['conditions']]):
+                paused = True
+                break
+        retries -= 1
+        # TODO(gyee): should we make the sleep time configurable?
+        time.sleep(30)
+    assert paused, 'Timed out while waiting for VM to be paused.'
+
+
+def _unpause_vm(admin_session, harvester_api_endpoints, basic_vm):
+    resp = admin_session.post(harvester_api_endpoints.unpause_vm % (
+        basic_vm['metadata']['name']))
+    assert resp.status_code == 204, 'Failed to unpause VM instance %s' % (
+        basic_vm['metadata']['name'])
+    ready = False
+    # VM startup is asynchronous so make sure it is running and it has a
+    # corresponding instance.
+    retries = 10
+    while retries:
+        resp = admin_session.get(harvester_api_endpoints.get_vm % (
+            basic_vm['metadata']['name']))
+        if resp.status_code == 200:
+            resp_json = resp.json()
+            if ('status' in resp_json and 'ready' in resp_json['status'] and
+                    resp_json['status']['ready']):
+                ready = True
+                break
+        retries -= 1
+        # TODO(gyee): should we make the sleep time configurable?
+        time.sleep(30)
+    assert ready, 'Timed out while waiting for VM to be unpaused.'
+
+
+def _update_vm(admin_session, harvester_api_endpoints, basic_vm):
+    domain_data = basic_vm['spec']['template']['spec']['domain']
+    domain_data['cpu']['cores'] = 2
+    domain_data['resources']['requests']['memory'] = "2Gi"
+
+    retries = 20
+    while retries:
+        resp = admin_session.put(harvester_api_endpoints.update_vm % (
+            basic_vm['metadata']['name']), json=basic_vm)
+        if (resp.status_code == 409 and
+                'latest version and try' in resp.content.decode('utf-8')):
+            time.sleep(30)
+            retries -= 1
+        else:
+            break
+    assert resp.status_code == 200, 'Failed to update vm: %s' % (
+        resp.content)
+    updated_vm = resp.json()
+    updated_domain_data = json.loads(updated_vm['spec']['template']['spec']
+                                     ['domain'])
+    assert updated_domain_data['cpu']['cores'] == domain_data['cpu']['cores']
+    assert (updated_domain_data['resources']['requests']['memory'] ==
+            domain_data['resources']['requests']['memory'])
