1912a32285d49b74db266adacb47094002ab52db
guangyee <gyee@suse.com>
2021-07-27 09:48:45 -0700

    Add test cases for VM networking
    1) test change management network to external VLAN
    2) test change external VLAN to management network
    3) update test code to no longer look for first-login password reset
       as that API is unstable. Also, doesn't appear Harvester API is enforcing
       first-login password reset.
    4) update the external host management scripts to accept both hostname and IP
       of the Harvester node

--- harvester_e2e_tests/fixtures/vm.py
+++ harvester_e2e_tests/fixtures/vm.py
@@ -129,6 +129,23 @@ def vms_with_same_vlan(request, admin_session, image, keypair,
 
 
 @pytest.fixture(scope='class')
+def vms_with_vlan_as_default_network(request, admin_session, image, keypair,
+                                     user_data_with_guest_agent, network_data,
+                                     harvester_api_endpoints, network):
+    vm_json = utils.create_vm(request, admin_session, image,
+                              harvester_api_endpoints,
+                              keypair=keypair,
+                              template='vm_with_vlan_as_default_network',
+                              network=network,
+                              network_data=network_data,
+                              user_data=user_data_with_guest_agent)
+    yield vm_json
+    if not request.config.getoption('--do-not-cleanup'):
+        utils.delete_vm(request, admin_session, harvester_api_endpoints,
+                        vm_json)
+
+
+@pytest.fixture(scope='class')
 def vm_with_volume(request, admin_session, image, volume, keypair,
                    harvester_api_endpoints):
     vm_json = utils.create_vm(request, admin_session, image,
