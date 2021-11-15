6b667a93b493bc771d8ecfc1fe667aa406b07119
yarunachalam <yarunachalam@suse.com>
2021-11-15 19:20:42 +0100

    Include markers for Authentication test cases Include markers for test cases based on priority. Include comment with test case number and description

--- harvester_e2e_tests/conftest.py
+++ harvester_e2e_tests/conftest.py
@@ -226,6 +226,10 @@ def pytest_configure(config):
     config.addinivalue_line(
         "markers", ('volumes_p2: mark test to run only P2 test for Volume')
     )
+    config.addinivalue_line(
+        "markers", ('authentication_p1: mark test to run only P1 '
+                    'test for authentication')
+    )
 
 
 def pytest_collection_modifyitems(config, items):
