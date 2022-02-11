3115df0475789c8eaab03d12e77fac6d67abfb6b
Noah Gildersleeve <noah.gildersleeve@suse.com>
2022-02-11 14:49:20 -0800

    added test skeletons and updated typedoc.json to only check the test specs
    updated typedoc to include all new tests. Also removed erroneous pageobject
    
    updated test skels and added in docs for specs and moved the folders. Updated version of typedoc in package.json
    
    updated typedoc version
    
    re-added entryPointStrategy to typedoc.json
    
    fixed newlines at end of files

---
+++ cypress/skel/skel.spec.ts
@@ -0,0 +1,38 @@
+import { LoginPage } from "../pageobjects/login.po";
+import { SettingsPage } from "../pageobjects/settings.po";
+const login = new LoginPage();
+const settings = new SettingsPage();
+
+/**
+ * 1. Login to the page
+ * 2. Edit the Type
+ * 3. Save the Changes
+ * 4. Validate that the changes saved
+ */
+export function testSkelTest() {}
+describe('Test Skel Page', () => {
+    it('Change the type for test skel page', () => {
+        login.login();
+        settings.changeUiSourceType(1);
+    });
+});
+
+/**
+ * 1. Login
+ * 2. Change Password
+ * 3. Log out
+ * 4. Login with new Password
+ * @notImplemented
+ */
+export function changePassword() {};
+
+/**
+ * 1. Log in as admin
+ * 2. Navigate to user admin page
+ * 3. Delete user
+ * 4. Log out
+ * 5. Try to log in as deleted user
+ * 6. Verify that login fails
+ * @notImplemented
+ */
+export function deleteUser() {};
---
+++ cypress/testcases/hosts.spec.ts
@@ -0,0 +1,17 @@
+import { HostsPage } from "../pageobjects/hosts.po";
+import { EditYamlPage } from "../pageobjects/editYaml.po";
+import { LoginPage } from "../pageobjects/login.po";
+const hosts = new HostsPage();
+const editYaml = new EditYamlPage();
+const login = new LoginPage();
+
+/**
+ * This will insert custom YAML into the hosts page while editing
+ */
+export function insertCustomYAML() {}
+it('should insert custom name into YAML', () => {
+    login.login();
+    hosts.navigateHostsPage();
+    hosts.editHostsYaml();
+    editYaml.insertCustomName('This is a custom name');
+    });
