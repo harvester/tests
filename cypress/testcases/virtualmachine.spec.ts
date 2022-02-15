c7f1c2c38bfb2efe144db84103add19612b94216
n313893254 <n313893254@126.com>
2022-02-15 14:23:09 +0800

    Cypress: Add check VM config and yaml

--- cypress/testcases/virtualmachine.spec.ts
+++ cypress/testcases/virtualmachine.spec.ts
@@ -3,15 +3,19 @@ import { LoginPage } from "../pageobjects/login.po";
 
 const vms = new VmsPage();
 const login = new LoginPage();
+const VM_NAME = 'test-vm-name-automation'
 
 describe('VM Page', () => {
-  it('Create a vm with all the default values', () => {
+  beforeEach(() => {
     login.login();
+  });
+
+  it('Create a vm with all the default values', () => {
     vms.goToCreate();
 
     const defaultValue = {
       namespace: 'default',
-      name: 'test-vm-name-automation',
+      name: VM_NAME,
       description: 'test-vm-description-automation',
       cpu: '2',
       memory: '4',
@@ -22,4 +26,20 @@ describe('VM Page', () => {
 
     vms.save();
   });
+
+  it('Config and YAML should show', () => {
+    vms.goToList();
+    cy.get('.search').type(VM_NAME)
+    cy.contains(VM_NAME).click()
+
+    const config = cy.get('.masthead button').contains('Config')
+    expect(config.should('be.visible'));
+    config.click()
+    cy.url().should('contain', 'config')
+
+    const yaml = cy.get('.masthead button').contains('YAML')
+    expect(yaml.should('be.visible'));
+    yaml.click()
+    cy.url().should('contain', 'yaml')
+  })
 });
