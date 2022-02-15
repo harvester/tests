4ea7085d7d31bb448b0732a99b0dff71f4437c3f
n313893254 <n313893254@126.com>
2022-02-15 13:46:04 +0800

    Update for review comment

--- cypress/pageobjects/components/labeled-input.po.ts
+++ cypress/pageobjects/components/labeled-input.po.ts
@@ -1,8 +1,10 @@
 import ComponentPo from '@/pageobjects/components/component.po';
 
 export default class LabeledSelectPo extends ComponentPo {
-  input(string: string) {
-    this.self().type(string)
+  input(string: string | undefined) {
+    if (string) {
+      this.self().type(string)
+    }
 
     return
   }
