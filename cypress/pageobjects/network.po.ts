816c1e1eeef381096edb9642f77f9b21e47b6877
n313893254 <n313893254@126.com>
2022-02-23 16:12:16 +0800

    Add a network to an existing VM with only 1 network

---
+++ cypress/pageobjects/network.po.ts
@@ -0,0 +1,17 @@
+import PagePo from '@/utils/components/page.po';
+
+export default class NetworkPage extends PagePo {
+  static type: string = 'k8s.cni.cncf.io.networkattachmentdefinition'
+  static url: string = `${Cypress.env('baseUrl')}dashboard/c/local/harvester/${this.type}`
+
+  constructor() {
+    super(NetworkPage.url);
+  }
+
+  public goToList() {
+    cy.intercept('GET', `/v1/harvester/k8s.cni.cncf.io.network-attachment-definitions`).as('goToList');
+    cy.visit(NetworkPage.url);
+    cy.wait('@goToList')
+    expect(cy.url().should('eq', NetworkPage.url));
+  }
+}
---
+++ cypress/testcases/virtualmachines/network.spec.ts
@@ -0,0 +1,65 @@
+import NetworkPage from '@/pageobjects/network.po';
+import { VmsPage } from "@/pageobjects/virtualmachine.po";
+import { LoginPage } from "@/pageobjects/login.po";
+
+const network = new NetworkPage()
+const vms = new VmsPage();
+const login = new LoginPage();
+
+describe('Add a network to an existing VM with only 1 network', () => {
+  const VM_NAME = 'test-network'
+  const NETWORK_1 = 'vlan178'
+  const NETWORK_2 = 'vlan188'
+
+  beforeEach(() => {
+    login.login();
+  });
+
+  it('Step 1: VM should start successfully', () => {
+    const value = {
+      name: VM_NAME,
+      cpu: '2',
+      memory: '4',
+      image: 'ubuntu-18.04-server-cloudimg-amd64.img',
+      networks: [{
+        network: 'vlan178',
+      }]
+    }
+    vms.create(value);
+    vms.goToConfigDetail(VM_NAME);
+    cy.url().should('contain', VM_NAME)
+  })
+
+  it('Step 2: The already existing network connectivity should still work', () => {
+    network.goToList()
+
+    cy.get('.search').type(NETWORK_1)
+    const col = cy.contains(NETWORK_1)
+    expect(col.should('be.visible'))
+    expect(col.parentsUntil('tbody', 'tr').contains('Active').should('be.visible'))
+  })
+
+  it('Step 2: Add a network to the VM', () => {
+    const value = {
+      networks: [{
+        network: 'vlan178',
+      }, {
+        network: 'vlan188',
+      }]
+    }
+    vms.edit(VM_NAME, value);
+  })
+
+  it('Step 4: The new connectivity should also work', () => {
+    network.goToList()
+
+    cy.get('.search').type(NETWORK_2)
+    const col = cy.contains(NETWORK_2)
+    expect(col.should('be.visible'))
+    expect(col.parentsUntil('tbody', 'tr').contains('Active').should('be.visible'))
+  })
+
+  it('Step 3: Delete VM', () => {
+    vms.delete(VM_NAME)
+  })
+})
