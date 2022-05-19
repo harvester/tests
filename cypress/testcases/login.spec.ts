2c65cb1f75cac7e2f9945c1df14b0f0f54813259
Lanfon Fan <lanfon.fan@suse.com>
2022-05-19 19:53:16 +0800

    + [CY] scaffold first time login test cases

--- cypress/testcases/login.spec.ts
+++ cypress/testcases/login.spec.ts
@@ -1,4 +1,34 @@
+import { onlyOn } from "@cypress/skip-test";
 import { LoginPage } from "@/pageobjects/login.po";
+
+
+describe("First Time Login Page", () => {
+  let isFirstTimeLogin: boolean = false;
+  before(async () => {
+    isFirstTimeLogin = await LoginPage.isFirstTimeLogin();
+  })
+
+  context("Check Options", () => {
+    it("is required to accept term", () => {
+      onlyOn(isFirstTimeLogin);
+    });
+
+    it("is optional to accept collection of anonymous statstics", () => {
+      onlyOn(isFirstTimeLogin);
+    });
+  });
+
+  context("Set Password", () => {
+    specify("Password inconsistant", () => {
+      onlyOn(isFirstTimeLogin);
+    });
+
+    specify("Password consistant", () => {
+      onlyOn(isFirstTimeLogin);
+    });
+  });
+});
+
 
 /**
  * This is the login spec
