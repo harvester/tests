4d6009c9192fa3d61f62002e97fb0afdab5c29b3
Lanfon Fan <lanfon.fan@suse.com>
2022-05-22 22:03:36 +0800

    + [CY] implement login test cases

--- cypress/testcases/login.spec.ts
+++ cypress/testcases/login.spec.ts
@@ -11,20 +11,56 @@ describe("First Time Login Page", () => {
   context("Check Options", () => {
     it("is required to accept term", () => {
       onlyOn(isFirstTimeLogin);
+      const page = new LoginPage();
+      page.visit();
+
+      page.checkEula(true);
+      expect(page.submitBtn.should('be.enabled'));
+
+      page.checkEula(false);
+      expect(page.submitBtn.should("be.disabled"));
+
     });
 
     it("is optional to accept collection of anonymous statstics", () => {
       onlyOn(isFirstTimeLogin);
+      const page = new LoginPage();
+      page.visit();
+
+      page.checkTelemetry(true);
+      expect(page.submitBtn.should("be.disabled"));
+
+      page.checkTelemetry(false);
+      expect(page.submitBtn.should("be.disabled"));
+
     });
   });
 
   context("Set Password", () => {
     specify("Password inconsistant", () => {
       onlyOn(isFirstTimeLogin);
+      const page = new LoginPage();
+      page.visit();
+      page.selectSpecificPassword();
+      page.checkEula(true);
+
+      page.inputPassword("abcd1234");
+      expect(page.submitBtn.should("be.disabled"));
+
+      page.inputPassword(page.password, "abcd1234");
+      expect(page.submitBtn.should("be.disabled"));
+
     });
 
     specify("Password consistant", () => {
       onlyOn(isFirstTimeLogin);
+      const page = new LoginPage();
+      page.visit();
+      page.selectSpecificPassword();
+      page.checkEula(true);
+
+      page.inputPassword(page.password, page.password);
+      expect(page.submitBtn.should("be.enabled"));
     });
   });
 });
@@ -36,13 +72,13 @@ describe("First Time Login Page", () => {
  * 2. Login with already set password
  */
 describe('login page for harvester', () => {
-    it('should login the first time', () => {
-        const login = new LoginPage();
-        login.firstLogin();
-    });
+    // it('should login the first time', () => {
+    //     const login = new LoginPage();
+    //     login.firstLogin();
+    // });
     
-    it('should login successfully', () => {
-        const login = new LoginPage();
-        login.login();
-    });
+    // it('should login successfully', () => {
+    //     const login = new LoginPage();
+    //     login.login();
+    // });
 });
