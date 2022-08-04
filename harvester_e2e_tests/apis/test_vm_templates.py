ae90ffa2564d531888914ff02a093bf1b8d83f76
Lanfon Fan <lanfon.fan@suse.com>
2022-08-04 16:14:15 +0800

    * [py-e2e] update default templates counts

--- harvester_e2e_tests/apis/test_vm_templates.py
+++ harvester_e2e_tests/apis/test_vm_templates.py
@@ -22,13 +22,15 @@ pytest_plugins = [
    'harvester_e2e_tests.fixtures.session',
   ]
 
+DEFAULT_TEMPLATES = 4
+
 
 def test_verify_default_vm_templates(admin_session, harvester_api_endpoints):
     resp = admin_session.get(harvester_api_endpoints.list_vm_templates)
     assert resp.status_code == 200, (
         'Failed to list virtual machine templates: %s' % (resp.content))
     vm_templates = resp.json()
-    assert len(vm_templates['items']) == 3
+    assert len(vm_templates['items']) == DEFAULT_TEMPLATES
 
 
 def test_verify_default_vm_template_versions(admin_session,
@@ -38,7 +40,7 @@ def test_verify_default_vm_template_versions(admin_session,
         'Failed to list virtual machine template versions: %s' % (
             resp.content))
     vm_template_versions = resp.json()
-    assert len(vm_template_versions['items']) == 3
+    assert len(vm_template_versions['items']) == DEFAULT_TEMPLATES
 
 
 def test_create_verify_vm_template(vm_template_version):
