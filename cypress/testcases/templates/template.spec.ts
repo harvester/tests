0c5eea4391efcf0ff58e8a038e741bed52ae0303
yuzeng <cridequeger@yahoo.com>
2022-11-29 16:33:01 +0800

    [CY] add the case of deleting default template version

--- cypress/testcases/templates/template.spec.ts
+++ cypress/testcases/templates/template.spec.ts
@@ -1,3 +1,4 @@
+import { HCI, SECRET } from '@/constants/types';
 import { generateName } from '@/utils/utils';
 import templatePage from "@/pageobjects/template.po";
 
@@ -18,15 +19,72 @@ describe('Create a vm template with all the required values', () => {
   });
 
   it('Create a vm template with the required values', () => {
-    const value = {
-      name: NAME,
-      cpu: '2',
-      memory: '4',
-      namespace,
-    }
+    templates.goToList();
+    templates.setNameNsDescription(NAME, namespace);
+    templates.setBasics('2', '4');
+    templates.save(namespace);
+    templates.delete(namespace, NAME);
+  });
+});
 
-    templates.create(value, true);
+/**
+ * 1. Go to Advanced -> Templates
+   2. Create a new template
+   3. Modify the template to create a new version
+   4. Click the config button of the default version template
+   5. Click the config button of the non default version template
+*/
+describe.only('Delete VM template default version', () => {
+  const NAME = generateName('test-template');
+  const namespace = 'default';
+  let interceptionCount = 0;
 
-    templates.delete(namespace, NAME)
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it('Create a vm template with the required values', () => {
+    cy.intercept('POST', `v1/harvester/${HCI.VM_VERSION}s/default`).as('create');
+
+    templates.goToList();
+    templates.goToCreate();
+    templates.setNameNsDescription(NAME, namespace);
+    templates.setBasics('2', '4');
+    templates.save(namespace);
+
+    cy.wait('@create').should((res: any) => {
+      expect(res.response?.statusCode, 'Create VM Template').to.equal(201);
+      const version = res?.response?.body || {}
+      const versionName = version?.metadata?.name || '';
+
+      templates.hasAction({
+        name: versionName,
+        ns: namespace,
+        action: 'Delete',
+        expect: false,
+      })
+
+      templates.clickAction(versionName, 'Modify template (Create new version)');
+    });
+    
+    
+    cy.intercept('POST', `v1/harvester/${HCI.VM_VERSION}s/default`).as('create');
+    
+    templates.setBasics('1', '2');
+    templates.save(namespace, 'Save');
+
+    cy.wait('@create').should((res: any) => {
+      expect(res.response?.statusCode, 'Create VM Template').to.equal(201);
+      const version = res?.response?.body || {}
+      const versionName = version?.metadata?.name || '';
+
+      templates.hasAction({
+        name: versionName,
+        ns: namespace,
+        action: 'Delete',
+      })
+    });
+
+    templates.delete(namespace, NAME);
   });
 });
