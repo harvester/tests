0e4c21ae1728eb296e1ccb55fa0fcd6b2115488d
davidtclin <davidtclin58@gmail.com>
2023-03-03 11:26:14 +0800

    Update the following content: (23/03/03) 1. Fix the frequently calling in export volume 2. Move one time used function in to test case content

--- harvester_e2e_tests/integration/test_images.py
+++ harvester_e2e_tests/integration/test_images.py
@@ -13,21 +13,6 @@ pytest_plugins = [
 ]
 
 
-@pytest.mark.images
-@pytest.mark.p0
-@pytest.mark.dependency(name="create_image_url")
-def test_create_with_url(api_client, image_opensuse, unique_name, wait_timeout, sleep_timeout):
-    """
-    Test if you can create an image from a URL.
-
-
-@pytest.fixture(scope='session')
-def iso_image_url():
-    base_url = "https://github.com/rancher/k3os/releases/download/v0.20.11-k3s2r1/"
-    image_name = "k3os-amd64.iso"
-    return base_url + image_name
-
-
 @pytest.fixture(scope='class')
 def qcow2_name(unique_name):
     name = 'qcow2-' + unique_name
@@ -269,19 +254,9 @@ class TestBackendImages:
         get_image(api_client, unique_name)
         delete_image(api_client, unique_name, wait_timeout)
 
-    @pytest.mark.images_p1
-    def test_create_invalid_url(self, api_client, unique_name):
-        """
-        Test create image from invalid url
-
-        Steps:
-        1. Create image with invalid URL. e.g. - https: // test.img
-        2. Check the Image state show as Failed
-        """
-        self.create_with_invalid_url(api_client, unique_name)
-
-    @pytest.mark.images_p1
-    def test_create_invalid_file(self, api_client):
+    @ pytest.mark.p0
+    def test_create_invalid_file(self, api_client, unique_name,
+                                 fake_invalid_image_file, wait_timeout):
         """
         Test create upload image from invalid file type
 
@@ -353,37 +328,8 @@ class TestBackendImages:
 
         }
 
-        self.update_by_input(api_client, 'k3os', updates)
-
-    @pytest.mark.images_p1
-    @pytest.mark.dependency(name="edit_image_not_in_use")
-    def test_edit_image_not_in_use(self, api_client, qcow2_image_url, iso_image_url):
-        """
-        Test can edit image which not in use
-
-        Steps:
-        1. Check the image created from URL exists
-        2. Update the image labels and description
-        3. Check can change the image content
-        """
-
-        self.get_image(api_client, 'k3os')
-
-        updates = {
-            "labels": {
-                "usage-label": "no"
-            },
-            "annotations": {
-                "field.cattle.io/description": 'edit image not in use'
-            },
-
-        }
-
-        self.update_by_input(api_client, 'opensuse', updates)
-
-    def update_by_input(self, api_client, unique_name, updates):
-
-        code, data = api_client.images.update(unique_name, dict(metadata=updates))
+        # Update image by input
+        code, data = api_client.images.update(qcow2_name, dict(metadata=updates))
 
         assert 200 == code, (f"Failed to update image with error: {code}, {data}")
 
@@ -398,127 +344,7 @@ class TestBackendImages:
                       for f, k, v, n in unexpected)
         )
 
-    def upload_file(self, api_client, unique_name, file_name, size):
-        create_image_cmd = "qemu-img create " + file_name + " " + size
-        os.system(create_image_cmd)
-        resp = api_client.images.create_by_file(unique_name, file_name)
+        delete_volume(api_client, unique_name, wait_timeout)
 
-        assert 200 == resp.status_code, (
-            f"Failed to upload fake image with error:{resp.status_code}, {resp.content}"
-        )
-
-    def get_export(self, api_client, display_name):
-        code, data = api_client.images.get()
-        assert 200 == code, (code, data)
-
-        for item in data['items']:
-            if item['spec']['displayName'] == display_name:
-                image_name = item['metadata']['name']
-                break
-            else:
-                raise AssertionError(
-                    f"Failed to find image {display_name}"
-                )
-        return image_name
-
-    def create_image_url(self, api_client, display_name, image_url, wait_timeout):
-        code, data = api_client.images.create_by_url(display_name, image_url)
-
-        assert 201 == code, (code, data)
-        image_spec = data.get('spec')
-
-        assert display_name == image_spec.get('displayName')
-        assert "download" == image_spec.get('sourceType')
-
-        endtime = datetime.now() + timedelta(seconds=wait_timeout)
-
-        while endtime > datetime.now():
-            code, data = api_client.images.get(display_name)
-            image_status = data.get('status', {})
-
-            assert 200 == code, (code, data)
-            if image_status.get('progress') == 100:
-                break
-            sleep(5)
-        else:
-            raise AssertionError(
-                f"Failed to download image {display_name} with {wait_timeout} timed out\n"
-                f"Still got {code} with {data}"
-            )
-
-    def delete_image(self, api_client, unique_name, wait_timeout):
-        code, data = api_client.images.delete(unique_name)
-
-        assert 200 == code, (f"Failed to delete image with error: {code}, {data}")
-
-        endtime = datetime.now() + timedelta(seconds=wait_timeout)
-
-        while endtime > datetime.now():
-            code, data = api_client.images.get(unique_name)
-            if code == 404:
-                break
-            sleep(5)
-        else:
-            raise AssertionError(
-                f"Failed to delete image {unique_name} with {wait_timeout} timed out\n"
-                f"Still got {code} with {data}"
-            )
-
-    def delete_volume(self, api_client, unique_name, wait_timeout):
-        code, data = api_client.volumes.delete(unique_name)
-
-        assert 200 == code, (f"Failed to delete volume with error: {code}, {data}")
-
-        endtime = datetime.now() + timedelta(seconds=wait_timeout)
-        while endtime > datetime.now():
-            code, data = api_client.volumes.get(unique_name)
-            if code == 404:
-                break
-            sleep(5)
-        else:
-            raise AssertionError(
-                f"Failed to delete volume {unique_name} with {wait_timeout} timed out\n"
-                f"Still got {code} with {data}"
-            )
-
-    def get_image(self, api_client, unique_name):
-        code, data = api_client.images.get()
-
-        assert len(data['items']) > 0, (code, data)
-
-    1. Create an image from URL.
-    2. Check for 201 response.
-    3. loop until the image has conditions.
-    4. Check if the image is intialzied and the status is true
-    5. Remove image
-    """
-    code, data = api_client.images.create_by_url(unique_name, image_opensuse.url)
-    assert 201 == code, (
-        f"Failed to create image {unique_name} from URL got\n"
-        f"Creation got {code} with {data}"
-    )
-    endtime = datetime.now() + timedelta(wait_timeout)
-    while endtime > datetime.now():
-        code, data = api_client.images.get(unique_name)
-        assert 200 == code, (code, data)
-        assert unique_name == data['metadata']['name']
-
-    def create_with_invalid_url(self, api_client, unique_name):
-        code, data = api_client.images.create_by_url(unique_name, f"https://{unique_name}.img")
-
-        assert 201 == code, (code, data)
-
-        endtime = datetime.now() + timedelta(minutes=3)
-        while endtime > datetime.now():
-            code, data = api_client.images.get(unique_name)
-            image_conds = data.get('status', {}).get('conditions', [])
-            if len(image_conds) > 0:
-                break
-            sleep(3)
-
-        assert len(image_conds) == 1, f"Got unexpected image conditions!\n{data}"
-        assert "Initialized" == image_conds[0].get("type")
-        assert "False" == image_conds[0].get("status")
-        assert "no such host" in image_conds[0].get("message")
-
-        api_client.images.delete(unique_name)
+        delete_image(api_client, qcow2_name, wait_timeout)
+        delete_image(api_client, iso_name, wait_timeout)
