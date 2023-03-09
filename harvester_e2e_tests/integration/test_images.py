7501a4c172011e0851b996982e4527e893e507ab
davidtclin <davidtclin58@gmail.com>
2023-03-10 00:27:44 +0800

    Fix failed test result on used Jenkins environment 23/03/10

--- harvester_e2e_tests/integration/test_images.py
+++ harvester_e2e_tests/integration/test_images.py
@@ -9,7 +9,12 @@ import pytest
 
 
 pytest_plugins = [
-    "harvester_e2e_tests.fixtures.api_client",
+    'harvester_e2e_tests.fixtures.api_endpoints',
+    'harvester_e2e_tests.fixtures.api_client',
+    'harvester_e2e_tests.fixtures.volume',
+    'harvester_e2e_tests.fixtures.session',
+    'harvester_e2e_tests.fixtures.image',
+    'harvester_e2e_tests.fixtures.images',
 ]
 
 
@@ -179,15 +184,18 @@ class TestBackendImages:
                 if image['spec']['displayName'] == unique_name:
                     if 100 == image.get('status', {}).get('progress', 0):
                         image_id = image['metadata']['name']
-                        # return image['metadata']['name']
-                    sleep(3)  # snooze
-                else:
-                    raise AssertionError(
-                        f"Failed to find image {unique_name}"
-                    )
+                    break
+            else:
+                raise AssertionError(
+                    f"Failed to find image {unique_name}"
+                )
 
             if image_id != "":
                 break
+
+            sleep(3)  # snooze
+
+        get_volume(api_client, unique_name)
 
         delete_volume(api_client, unique_name, wait_timeout)
         delete_image(api_client, image_id, wait_timeout)
@@ -348,3 +356,36 @@ class TestBackendImages:
 
         delete_image(api_client, qcow2_name, wait_timeout)
         delete_image(api_client, iso_name, wait_timeout)
+
+
+@pytest.mark.images
+@pytest.mark.p0
+@pytest.mark.dependency(name="create_image_url")
+def test_create_with_url(api_client, image_opensuse, unique_name, wait_timeout, sleep_timeout):
+    """
+    Test if you can create an image from a URL.
+    Prerequisite:
+    Setting opensuse-image-url set to a valid URL for
+    an opensuse image.
+    1. Create an image from URL.
+    2. Check for 201 response.
+    3. loop until the image has conditions.
+    4. Check if the image is intialzied and the status is true
+    5. Remove image
+    """
+    code, data = api_client.images.create_by_url(unique_name, image_opensuse.url)
+    assert 201 == code, (
+        f"Failed to create image {unique_name} from URL got\n"
+        f"Creation got {code} with {data}"
+    )
+    endtime = datetime.now() + timedelta(wait_timeout)
+    while endtime > datetime.now():
+        code, data = api_client.images.get(unique_name)
+        image_conds = data.get('status', {}).get('conditions', [])
+        if len(image_conds) > 0:
+            break
+        sleep(sleep_timeout)
+
+    assert "Initialized" == image_conds[-1].get("type")
+    assert "True" == image_conds[-1].get("status")
+    api_client.images.delete(unique_name)
