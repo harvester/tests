e70b3921247bda92dc68c5634e62901eb5757343
Lanfon Fan <lanfon.fan@suse.com>
2023-05-12 03:52:56 +0800

    + [apiclient] add 3 new rancher API managers

--- apiclient/rancher_api/managers.py
+++ apiclient/rancher_api/managers.py
@@ -2,6 +2,9 @@ import base64
 import json
 import yaml
 from weakref import ref
+from collections.abc import Mapping
+
+from .models import UserSpec
 
 
 DEFAULT_NAMESPACE = "default"
@@ -58,6 +61,73 @@ class BaseManager:
 
     def _delete(self, path, *, raw=False, **kwargs):
         return self._delegate("_delete", path, raw=raw, **kwargs)
+
+
+class UserManager(BaseManager):
+    PATH_fmt = "v3/users/{uid}"
+    ROLE_fmt = "v3/globalrolebindings/{uid}"
+
+    Spec = UserSpec
+
+    def get(self, uid="", *, raw=False, **kwargs):
+        path = self.PATH_fmt.format(uid=uid)
+        return self._get(path, raw=raw, **kwargs)
+
+    def get_by_name(self, name, *, raw=False):
+        resp = self.get(raw=raw, params=dict(username=name))
+        if raw:
+            return resp
+        try:
+            code, data = resp
+            return code, data['data'][0]
+        except IndexError:
+            return 404, dict(type="error", status=404, code="NotFound",
+                             message=f"username {name!r} not found")
+
+    def create(self, username, spec, *, raw=False):
+        if isinstance(spec, self.Spec):
+            spec = spec.to_dict(username)
+        path = self.PATH_fmt.format(uid="")
+        return self._create(path, json=spec, raw=raw)
+
+    def update(self, uid, spec, *, raw=False, as_json=True, **kwargs):
+        path = self.PATH_fmt.format(uid=uid)
+        _, user = self.get(uid)
+        if isinstance(spec, self.Spec):
+            spec = spec.to_dict(user['username'])
+        if isinstance(spec, Mapping) and as_json:
+            spec = merge_dict(spec, user)
+        return self._update(path, spec, raw=raw, as_json=as_json, **kwargs)
+
+    def update_password(self, uid, passwd, *, raw=False):
+        path = self.PATH_fmt.format(uid=uid)
+        data = dict(newPassword=passwd)
+        return self._create(path, raw=raw, json=data, params=dict(action="setpassword"))
+
+    def delete(self, uid, *, raw=False):
+        path = self.PATH_fmt.format(uid=uid)
+        return self._delete(path, raw=raw)
+
+    def get_roles(self, uid, *, raw=False, **kwargs):
+        path = self.ROLE_fmt.format(uid="")
+        params = merge_dict(dict(userId=uid), kwargs.pop('params', {}))
+        return self._get(path, params=params, raw=raw, **kwargs)
+
+    def add_role(self, uid, role_id, *, raw=False):
+        path = self.ROLE_fmt.format(uid="")
+        data = dict(type="globalRoleBinding", userId=uid, globalRoleId=role_id)
+        return self._create(path, json=data, raw=raw)
+
+    def delete_role(self, uid, role_id, *, raw=False):
+        try:
+            code, data = self.get_roles(uid, params=dict(globalRoleId=role_id))
+            ruid = data['data'][0]['id']
+            return self._delete(self.ROLE_fmt.format(uid=ruid), raw=raw)
+        except IndexError:
+            return 404, dict(type='error', status=404, code='NotFound',
+                             message=f"User {uid!r} haven't Role {role_id!r}")
+        except KeyError:
+            return code, data
 
 
 class SettingManager(BaseManager):
---
+++ apiclient/rancher_api/models.py
@@ -0,0 +1,43 @@
+# from copy import deepcopy
+# from json import dumps, loads
+
+# import yaml
+
+
+class UserSpec:
+    _data = None
+
+    def __init__(self, password, display_name="", description="",
+                 change_pwd=False, enabled=True, permissions=None):
+        self.password = password
+        self.display_name = display_name
+        self.change_pwd = change_pwd
+        self.description = description
+        self.enabled = enabled
+
+    def to_dict(self, username):
+        data = {
+            "type": "user",
+            "enabled": self.enabled,
+            "username": username,
+            "mustChangePassword": self.change_pwd,
+            "password": self.password
+        }
+
+        if self.display_name:
+            data['name'] = self.display_name
+        if self.description:
+            data['description'] = self.description
+
+        return data
+
+    @classmethod
+    def from_dict(cls, data):
+        obj = cls(**dict(
+            display_name=data.get('name'),
+            description=data.get('description'),
+            change_pwd=data.get('mustChangePassword'),
+            enabled=data.get('enabled'),
+        ))
+        obj._data = data
+        return obj
