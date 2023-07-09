4bde5df1325a93f462aa1f1e4310c56d784495f2
Lanfon Fan <lanfon.fan@suse.com>
2023-07-09 23:58:55 +0800

    * [e2e] fix potential bug in power off/on

--- harvester_e2e_tests/integration/test_hosts.py
+++ harvester_e2e_tests/integration/test_hosts.py
@@ -124,68 +124,77 @@ def vm_force_reset_policy(api_client):
 @pytest.mark.hosts
 @pytest.mark.p1
 @pytest.mark.host_management
-@pytest.mark.dependency(name="host_poweroff")
-def test_host_poweroff_state(api_client, host_state, wait_timeout, available_node_names):
-    """
-    Test the hosts are the nodes which make the cluster
-    Covers:
-        hosts-01-Negative test-Verify the state for Powered down node
-    """
-    assert 1 < len(available_node_names), (
-        f"Cluster has {len(available_node_names)} available node, not enough for poweroff test."
-    )
+class TestHostState:
+    @pytest.mark.dependency(name="host_poweroff")
+    def test_poweroff_state(api_client, host_state, wait_timeout, available_node_names):
+        """
+        Test the hosts are the nodes which make the cluster
+        Covers:
+            hosts-01-Negative test-Verify the state for Powered down node
+        """
+        assert 2 <= len(available_node_names), (
+            f"The cluster only have {len(available_node_names)} available node."
+            " It's not enough for power off test."
+        )
+        _, node = api_client.hosts.get(available_node_names[-1])
 
-    _, nodes_info = api_client.hosts.get()
-    node = nodes_info['data'][-1]
-    node_ip = next(val["address"] for val in node['status']['addresses']
-                   if val["type"] == "InternalIP")
+        node_ip = next(val["address"] for val in node['status']['addresses']
+                       if val["type"] == "InternalIP")
 
-    rc, out, err = host_state.power(node['id'], node_ip, on=False)
-    assert rc == 0, (f"Failed to PowerOff node {node['id']} with error({rc}):\n"
-                     f"stdout: {out}\n\nstderr: {err}")
-    sleep(host_state.delay)  # Wait for the node to disappear
-    endtime = datetime.now() + timedelta(seconds=wait_timeout)
-    while endtime > datetime.now():
-        code, metric = api_client.hosts.get_metrics(node['id'])
-        if 404 == code:
-            break
-        sleep(5)
-    else:
-        raise AssertionError(f"Node {node['id']} still available after PowerOff script executed"
-                             f", script path: {host_state.path}")
+        rc, out, err = host_state.power(node['id'], node_ip, on=False)
+        assert rc == 0, (f"Failed to PowerOff node {node['id']} with error({rc}):\n"
+                         f"stdout: {out}\n\nstderr: {err}")
+        sleep(host_state.delay)  # Wait for the node to disappear
+        endtime = datetime.now() + timedelta(seconds=wait_timeout)
+        while endtime > datetime.now():
+            code, metric = api_client.hosts.get_metrics(node['id'])
+            if 404 == code:
+                break
+            sleep(5)
+        else:
+            raise AssertionError(
+                f"Node {node['id']} still available after PowerOff script executed"
+                f", script path: {host_state.path}"
+            )
 
-    _, node = api_client.hosts.get(node['id'])
-    assert node["metadata"]["state"]["name"] in ("in-progress", "unavailable")
+        _, node = api_client.hosts.get(node['id'])
+        assert node["metadata"]["state"]["name"] in ("in-progress", "unavailable")
 
+    @pytest.mark.dependency(name="host_poweron", depends=["host_poweroff"])
+    def test_poweron_state(api_client, host_state, wait_timeout, available_node_names):
+        assert 2 <= len(available_node_names), (
+            f"The cluster only have {len(available_node_names)} available node."
+            " It's not enough for power on test."
+        )
+        _, node = api_client.hosts.get(available_node_names[-1])
 
-@pytest.mark.hosts
-@pytest.mark.p1
-@pytest.mark.host_management
-@pytest.mark.dependency(name="host_poweron", depends=["host_poweroff"])
-def test_host_poweron_state(api_client, host_state, wait_timeout):
-    _, nodes_info = api_client.hosts.get()
+        assert node['metadata']['state']['error'], (
+            f"The node {available_node_names[-1]} was not poweroff.\n"
+            f"Node Status: {node['metadata']['status']}"
+        )
 
-    node = nodes_info['data'][-1]
-    node_ip = next(val["address"] for val in node['status']['addresses']
-                   if val["type"] == "InternalIP")
+        node_ip = next(val["address"] for val in node['status']['addresses']
+                       if val["type"] == "InternalIP")
 
-    rc, out, err = host_state.power(node['id'], node_ip, on=True)
-    assert rc == 0, (f"Failed to PowerOn node {node['id']} with error({rc}):\n"
-                     f"stdout: {out}\n\nstderr: {err}")
-    sleep(host_state.delay)  # Wait for the node to disappear
-    endtime = datetime.now() + timedelta(seconds=wait_timeout)
-    while endtime > datetime.now():
-        _, metric = api_client.hosts.get_metrics(node['id'])
-        state = metric.get("metadata", {}).get("state", {})
-        if state and not state.get("error") and state.get('name') != 'unavailable':
-            break
-        sleep(5)
-    else:
-        raise AssertionError(f"Node {node['id']} still unavailable after PowerOn script executed"
-                             f", script path: {host_state.path}")
+        rc, out, err = host_state.power(node['id'], node_ip, on=True)
+        assert rc == 0, (f"Failed to PowerOn node {node['id']} with error({rc}):\n"
+                         f"stdout: {out}\n\nstderr: {err}")
+        sleep(host_state.delay)  # Wait for the node to disappear
+        endtime = datetime.now() + timedelta(seconds=wait_timeout)
+        while endtime > datetime.now():
+            _, metric = api_client.hosts.get_metrics(node['id'])
+            state = metric.get("metadata", {}).get("state", {})
+            if state and not state.get("error") and state.get('name') != 'unavailable':
+                break
+            sleep(5)
+        else:
+            raise AssertionError(
+                f"Node {node['id']} still unavailable after PowerOn script executed"
+                f", script path: {host_state.path}"
+            )
 
-    _, node = api_client.hosts.get(node['id'])
-    assert "active" == node["metadata"]["state"]["name"]
+        _, node = api_client.hosts.get(node['id'])
+        assert "active" == node["metadata"]["state"]["name"]
 
 
 @pytest.mark.hosts
