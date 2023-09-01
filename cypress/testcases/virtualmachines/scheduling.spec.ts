44d50e5a534dec6260d8368fadc7f253959382f8
wujun <897415845@qq.com>
2023-09-01 11:30:49 +0800

    [CY] VM scheduling on Specific node

--- cypress/testcases/virtualmachines/scheduling.spec.ts
+++ cypress/testcases/virtualmachines/scheduling.spec.ts
@@ -1,7 +1,5 @@
-import YAML from 'js-yaml'
 import { VmsPage } from "@/pageobjects/virtualmachine.po";
 import { HostsPage } from "@/pageobjects/hosts.po";
-import { generateName } from '@/utils/utils';
 
 const vms = new VmsPage();
 const hosts = new HostsPage();
@@ -9,34 +7,37 @@ const hosts = new HostsPage();
 /**
  * https://harvester.github.io/tests/manual/virtual-machines/vm_schedule_on_node/
  */
-// TODO: require two nodes
-describe.skip('VM scheduling on Specific node', () => { 
+describe('VM scheduling on Specific node', () => { 
   beforeEach(() => {
     cy.login();
   });
 
   it('Schedule VM on the Node which is Enable Maintenance Mode', () => {
-    const VM_NAME = generateName('vm-scheduling')
-    const NAMESPACE = 'default'
     const hostList = Cypress.env('host');
+    const hostNames: string[] = hostList.map(host => host.name);
+    const maintenanceNode = hostNames[0]
+    const filterMaintenanceNames = hostNames.filter(name => name !== maintenanceNode);
 
+    // Check whether all nodes can be selected
     vms.goToCreate();
     vms.selectSchedulingType({type: 'specific'});
-    vms.checkSpecificNodes({includeNodes: [hostList[0].name, hostList[1].name]});
+    vms.checkSpecificNodes({includeNodes: hostNames});
     
     hosts.goToList();
-    hosts.enableMaintenance(hostList[0].name);
+    hosts.enableMaintenance(hostNames[0]);
     
+    // Maintenance nodes should not be selected
     vms.goToCreate();
     vms.selectSchedulingType({type: 'specific'});
-    vms.checkSpecificNodes({includeNodes: [hostList[1].name], excludeNodes: [hostList[0].name]});
+    vms.checkSpecificNodes({includeNodes: filterMaintenanceNames, excludeNodes: [maintenanceNode]});
 
     hosts.goToList();
     hosts.clickAction(hostList[0].name, 'Disable Maintenance Mode');
 
+    // Check whether all nodes can be selected
     vms.goToCreate();
     vms.selectSchedulingType({type: 'specific'});
-    vms.checkSpecificNodes({includeNodes: [hostList[0].name, hostList[1].name]});
+    vms.checkSpecificNodes({includeNodes: hostNames});
   })
 })
 
