57fedff81d14b0f3c101f701d0ed83c7b125b328
Albin Sun <albin.sun@suse.com>
2023-09-11 19:11:56 +0800

    Clean LH backupvolumes in teardown

--- apiclient/harvester_api/managers.py
+++ apiclient/harvester_api/managers.py
@@ -1018,3 +1018,17 @@ class LonghornVolumeManager(BaseManager):
     def get(self, name="", namespace=DEFAULT_LONGHORN_NAMESPACE, *, raw=False, **kwargs):
         path = self.PATH_fmt.format(API_VERSION=self.API_VERSION, name=name, namespace=namespace)
         return self._get(path, raw=raw, **kwargs)
+
+
+class LonghornBackupVolumeManager(BaseManager):
+    API_VERSION = "longhorn.io/v1beta2"
+    PATH_fmt = "apis/{API_VERSION}/namespaces/{namespace}/backupvolumes/{name}"
+    API_PATH_fmt = "v1/harvester/longhorn.io.backupvolumes/{namespace}{name}"
+
+    def get(self, name="", namespace=DEFAULT_LONGHORN_NAMESPACE, *, raw=False, **kwargs):
+        path = self.PATH_fmt.format(API_VERSION=self.API_VERSION, name=name, namespace=namespace)
+        return self._get(path, raw=raw, **kwargs)
+
+    def delete(self, name="", namespace=DEFAULT_LONGHORN_NAMESPACE, *, raw=False):
+        path = self.API_PATH_fmt.format(name=f"/{name}", namespace=namespace)
+        return self._delete(path, raw=raw)
