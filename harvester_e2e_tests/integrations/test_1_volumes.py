d98a288d2007670017c2f3a324c0265257625562
Albin Sun <albin.sun@suse.com>
2024-03-26 10:24:59 +0800

    Check volume status after VM deletion

--- harvester_e2e_tests/integrations/test_1_volumes.py
+++ harvester_e2e_tests/integrations/test_1_volumes.py
@@ -88,20 +88,22 @@ def test_create_volume(api_client, unique_name, ubuntu_image, create_as, source_
     polling_for("volume do created",
                 lambda code, data: 200 == code and data['status']['phase'] == "Bound",
                 api_client.volumes.get, unique_name)
+
     code, data = api_client.volumes.get(unique_name)
-    mdata = data['metadata']
-    # created
+    mdata, annotations = data['metadata'], data['metadata']['annotations']
     assert 200 == code, (code, data)
     assert unique_name == mdata['name'], (code, data)
-    if source_type == "VM Image":
-        assert image_id == mdata['annotations']['harvesterhci.io/imageId'], (code, data)
-    else:
-        assert not mdata['annotations'].get('harvesterhci.io/imageId'), (code, data)
-    # bounded and detached
-    assert data['status']['phase'] == "Bound", (code, data)
+    # status
     assert not mdata['state']['error'], (code, data)
     assert not mdata['state']['transitioning'], (code, data)
-    assert not mdata['annotations'].get("harvesterhci.io/owned-by"), (code, data)
+    assert data['status']['phase'] == "Bound", (code, data)
+    # source
+    if source_type == "VM Image":
+        assert image_id == annotations['harvesterhci.io/imageId'], (code, data)
+    else:
+        assert not annotations.get('harvesterhci.io/imageId'), (code, data)
+    # attachment
+    assert not annotations.get("harvesterhci.io/owned-by"), (code, data)
 
     # teardown
     polling_for("volume do deleted", lambda code, _: 404 == code,
@@ -135,7 +137,7 @@ class TestVolumeWithVM:
                     lambda c, d: 404 == c,
                     api_client.vms.get, vm_name)
 
-    def test_delete_volume_bounded_on_existing_vm(self, api_client, ubuntu_vm, polling_for):
+    def test_delete_volume_on_existing_vm(self, api_client, ubuntu_image, ubuntu_vm, polling_for):
         """
         1. Create a VM with volume
         2. Delete volume should reply 422
@@ -159,7 +161,21 @@ class TestVolumeWithVM:
         code, data = api_client.volumes.delete(vol_name)
         assert 422 == code, f"Should fail to delete volume\n{code}, {data}"
 
-    def test_delete_volume_bounded_on_deleted_vm(self, api_client, ubuntu_vm, polling_for):
+        # Check Volume
+        code, data = api_client.volumes.get(vol_name)
+        mdata, annotations = data['metadata'], data['metadata']['annotations']
+        assert 200 == code, (code, data)
+        assert mdata['name'] == vol_name, (code, data)
+        # status
+        assert not mdata['state']['error'], (code, data)
+        assert not mdata['state']['transitioning'], (code, data)
+        assert data['status']['phase'] == "Bound", (code, data)
+        # source
+        assert ubuntu_image["id"] == annotations['harvesterhci.io/imageId'], (code, data)
+        # attachment
+        assert ubuntu_vm['id'] in annotations.get("harvesterhci.io/owned-by"), data
+
+    def test_delete_volume_on_deleted_vm(self, api_client, ubuntu_image, ubuntu_vm, polling_for):
         """
         1. Create a VM with volume
         2. Delete volume should reply 422
@@ -177,3 +193,17 @@ class TestVolumeWithVM:
 
         code, data = api_client.volumes.delete(vol_name)
         assert 200 == code, f"Fail to delete volume\n{code}, {data}"
+
+        # Check Volume
+        code, data = api_client.volumes.get(vol_name)
+        mdata, annotations = data['metadata'], data['metadata']['annotations']
+        assert 200 == code, (code, data)
+        assert mdata['name'] == vol_name, (code, data)
+        # status
+        assert not mdata['state']['error'], (code, data)
+        assert not mdata['state']['transitioning'], (code, data)
+        assert data['status']['phase'] == "Bound", (code, data)
+        # source
+        assert ubuntu_image["id"] == annotations['harvesterhci.io/imageId'], (code, data)
+        # attachment
+        assert not annotations.get("harvesterhci.io/owned-by"), (code, data)
