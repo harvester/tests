f91f188c2f1a24c743d5b64c986fdfabcbdd7ace
Lanfon Fan <lanfon.fan@suse.com>
2024-04-23 01:11:59 +0800

    + [e2e] new test case for #1215

---
+++ harvester_e2e_tests/apis/test_vms.py
@@ -0,0 +1,33 @@
+import pytest
+
+pytest_plugins = [
+   "harvester_e2e_tests.fixtures.api_client"
+  ]
+
+
+@pytest.mark.p0
+@pytest.mark.negative
+@pytest.mark.virtualmachines
+class TestVMNegative:
+    def test_get_not_exist(self, api_client, unique_name):
+        """
+        1. Tries to get a VM that doesn't exist
+        2. Checks that the get command gets a 404
+        """
+        code, data = api_client.vms.get(unique_name)
+
+        assert 404 == code, (code, data)
+        assert "NotFound" == data.get('code'), (code, data)
+
+    def test_delete_not_exist(self, api_client, unique_name):
+        """
+        1. Tries to delete a VM that doesn't exist
+        2. Checks that it gets a 404
+        """
+        try:
+            api_client.set_retries(1)
+            code, data = api_client.vms.delete(unique_name)
+            assert 404 == code, (code, data)
+            assert "NotFound" in data.get('code'), (code, data)
+        finally:
+            api_client.set_retries()
