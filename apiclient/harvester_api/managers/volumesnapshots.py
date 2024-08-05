271baca51345e71d57f5afa0553e59ba4cdc5ffa
Lanfon Fan <lanfon.fan@suse.com>
2024-08-06 01:03:19 +0800

    * [apiclient] update volumesnapshot naming and get method

--- apiclient/harvester_api/managers/volumesnapshots.py
+++ apiclient/harvester_api/managers/volumesnapshots.py
@@ -2,12 +2,13 @@ from .base import DEFAULT_NAMESPACE, BaseManager
 
 
 class VolumeSnapshotManager(BaseManager):
-    PATH_fmt = "v1/harvester/snapshot.storage.k8s.io.volumesnapshots/{ns}/{uid}"
+    _qs = "?filter=metadata.ownerReferences.kind=PersistentVolumeClaim"
+    PATH_fmt = "v1/harvester/snapshot.storage.k8s.io.volumesnapshots/{ns}/{uid}{qs}"
 
     def delete(self, name, namespace=DEFAULT_NAMESPACE, *, raw=False):
         path = self.PATH_fmt.format(uid=name, ns=namespace)
         return self._delete(path, raw=raw)
 
     def get(self, name="", namespace=DEFAULT_NAMESPACE, *, raw=False):
-        path = self.PATH_fmt.format(uid=name, ns=namespace)
+        path = self.PATH_fmt.format(uid=name, ns=namespace, qs=self._qs)
         return self._get(path, raw=raw)
