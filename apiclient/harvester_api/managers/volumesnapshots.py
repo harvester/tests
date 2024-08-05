614395e46ba176d1c5e6dcdd1afada70be0b5051
Lanfon Fan <lanfon.fan@suse.com>
2024-08-06 01:06:01 +0800

    + [e2e] add test cases for clone volume

--- apiclient/harvester_api/managers/volumesnapshots.py
+++ apiclient/harvester_api/managers/volumesnapshots.py
@@ -2,13 +2,19 @@ from .base import DEFAULT_NAMESPACE, BaseManager
 
 
 class VolumeSnapshotManager(BaseManager):
-    _qs = "?filter=metadata.ownerReferences.kind=PersistentVolumeClaim"
-    PATH_fmt = "v1/harvester/snapshot.storage.k8s.io.volumesnapshots/{ns}/{uid}{qs}"
+    PATH_fmt = "v1/harvester/snapshot.storage.k8s.io.volumesnapshots/{ns}/{uid}"
 
     def delete(self, name, namespace=DEFAULT_NAMESPACE, *, raw=False):
         path = self.PATH_fmt.format(uid=name, ns=namespace)
         return self._delete(path, raw=raw)
 
     def get(self, name="", namespace=DEFAULT_NAMESPACE, *, raw=False):
-        path = self.PATH_fmt.format(uid=name, ns=namespace, qs=self._qs)
-        return self._get(path, raw=raw)
+        path = self.PATH_fmt.format(uid=name, ns=namespace)
+        rv = self._get(path, raw=raw)
+        if not (name or raw):
+            _, ds = rv
+            ds['data'] = [
+                d for d in ds['data']
+                if d['metadata']['ownerReferences'][0]['kind'] == "PersistentVolumeClaim"
+            ]
+        return rv
