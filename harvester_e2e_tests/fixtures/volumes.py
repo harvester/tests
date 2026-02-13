cf1de30dc5476ed6e84ea08993ae38c8057fed18
asc5543 <asc5543@gmail.com>
2026-02-13 08:50:32 +0800

    Create wait_volumes_deleted for waiting deletion
    Signed-off-by: asc5543 <asc5543@gmail.com>

--- harvester_e2e_tests/fixtures/volumes.py
+++ harvester_e2e_tests/fixtures/volumes.py
@@ -1,6 +1,8 @@
 
 import pytest
 from .base import wait_until
+from datetime import datetime, timedelta
+from time import sleep
 
 pytest_plugins = ["harvester_e2e_tests.fixtures.api_client"]
 
@@ -31,5 +33,19 @@ def volume_checker(api_client, wait_timeout, sleep_timeout):
             if 200 == code and "degraded" == data['status']['robustness']:
                 return True, (code, data)
             return False, (code, data)
+
+        @wait_until(wait_timeout, sleep_timeout)
+        def wait_volumes_deleted(self, vol_names):
+            for vol_name in vol_names:
+                self.volumes.delete(name=vol_name)
+                endtime = datetime.now() + timedelta(seconds=wait_timeout)
+                while endtime > datetime.now():
+                    code, data = self.volumes.get(name=vol_name)
+                    if 404 == code:
+                        break
+                    sleep(sleep_timeout)
+                if not (code == 404):
+                    return False, (code, data)
+            return True, (code, data)
 
     return VolumeChecker()
